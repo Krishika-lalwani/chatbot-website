6d5f4eb5a67df2321465c600766905e409aaeccb
Krishika-lalwani <23mt0412@sxca.edu.in>
2026-03-06 16:08:01 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.set_page_config(page_title="My Chatbot")
+
+st.title("My AI Chatbot 🤖")
+
+st.write("Chat with my chatbot below.")
+
+botpress = """
+<iframe 
+src="https://cdn.botpress.cloud/webchat/v3.6/shareable.html?configUrl=https://files.bpcontent.cloud/2026/02/20/15/20260220151633-UWG3A038.json"
+width="100%"
+height="600">
+</iframe>
+"""
+
+components.html(botpress, height=600)
